f1b68fe705df8a69a729144cc2364fb0ace51db6
Phaneendra1429 <86067050+Phaneendra1429@users.noreply.github.com>
2022-03-08 14:53:17 +0530

    Update app.py

--- app.py
+++ app.py
@@ -14,9 +14,9 @@ import sklearn
 from sklearn.preprocessing import StandardScaler
 app = Flask(__name__)
 model = pickle.load(open('LR.pkl', 'rb'))
-@app.route('/')
-def home():
-    return "<center><h1>Electrical Fault detection </h1> </center>"
+@app.route('/',methods=['GET'])
+def Home():
+    return render_template('index.html')
 
 standard_to = StandardScaler()
 @app.route("/predict", methods=['GET','POST'])
